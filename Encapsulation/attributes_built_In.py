33671900542d25b9416e5d655f6a87a357b1faa6
NickPantev <nickpantev33@gmail.com>
2023-07-15 19:57:25 +0300

    changes

--- Encapsulation/attributes_built_In.py
+++ Encapsulation/attributes_built_In.py
@@ -2,6 +2,18 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+     
+    def info(self):
+        return f"Name: {self.name}, Age: {self.age}"  
         
-        
+p = Person("Guman", 29)
+
+c_name = input()
+
+c_value = getattr(p, c_name)
+if hasattr(c_value, '__call__'):
+    print(c_value())
+else:
+    print(c_value)
+
     
