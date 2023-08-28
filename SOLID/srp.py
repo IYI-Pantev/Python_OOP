f2094c7381fb040864e6cfe7236b0c9de21f1243
NickPantev <nickpantev33@gmail.com>
2023-08-28 11:55:14 +0300

    c

--- SOLID/srp.py
+++ SOLID/srp.py
@@ -5,24 +5,24 @@
  
 import math
  
-from shapes import Rect, Circle
+# from shapes import Rect, Circle
  
-class ShapesController:
-    # ! violation of SRP
-    # 1 calc of area 
-    # 2 printing the sum
-     def printing_areas_sum(self, shapes):
-         area_sum = 0
-         for shape in shapes:
-             if isinstance(shape, Rect):
-                 area_sum += shape.width * shape.height
-             elif isinstance(shape, Circle):
-                 area_sum += shape.radius * shape.radius * math.pi
+# class ShapesController:
+#     # ! violation of SRP
+#     # 1 calc of area 
+#     # 2 printing the sum
+#      def printing_areas_sum(self, shapes):
+#          area_sum = 0
+#          for shape in shapes:
+#              if isinstance(shape, Rect):
+#                  area_sum += shape.width * shape.height
+#              elif isinstance(shape, Circle):
+#                  area_sum += shape.radius * shape.radius * math.pi
                  
-         print(f"Area sum of shapes is: {area_sum: .2f}")
+#          print(f"Area sum of shapes is: {area_sum: .2f}")
          
 
 
-shapes = [Circle(10), Rect(4, 5)]
+# shapes = [Circle(10), Rect(4, 5)]
 
-ShapesController().printing_areas_sum(shapes)
+# ShapesController().printing_areas_sum(shapes)
